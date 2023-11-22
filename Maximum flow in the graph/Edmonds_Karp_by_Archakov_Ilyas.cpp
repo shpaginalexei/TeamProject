4bf40add07e8aa111be3c88433b0216caa3d1b1d
ilyass013 <150536146+ilyass013@users.noreply.github.com>
2023-11-22 15:20:05 +0300

    Edmons Karp Algorithm

--- Maximum flow in the graph/Edmonds_Karp_by_Archakov_Ilyas.cpp
+++ Maximum flow in the graph/Edmonds_Karp_by_Archakov_Ilyas.cpp
@@ -4,5 +4,48 @@
 using namespace std;
 
 int edmonds_karp_algorithm(Graph graph, int source, int sink) {
-    return 100;
+    int maxFlow = 0;
+
+    while (true) {
+        vector<int> parent(graph.V, -1);
+        vector<int> minCapacity(graph.V, 0);
+        minCapacity[source] = INT_MAX;
+
+        vector<bool> visited(graph.V, false);
+        visited[source] = true;
+
+        vector<int> queue;
+        int front = 0;
+        int rear = 0;
+        queue.push_back(source);
+
+        while (front <= rear) {
+            int u = queue[front++];
+            for (int v = 0; v < graph.V; v++) {
+                if (!visited[v] && graph.adj[u][v] > 0) {
+                    parent[v] = u;
+                    minCapacity[v] = min(minCapacity[u], graph.adj[u][v]);
+                    visited[v] = true;
+                    queue.push_back(v);
+                    rear++;
+                }
+            }
+        }
+
+        if (!visited[sink]) {
+            break;
+        }
+
+        maxFlow += minCapacity[sink];
+
+        int v = sink;
+        while (v != source) {
+            int u = parent[v];
+            graph.adj[u][v] -= minCapacity[sink];
+            graph.adj[v][u] += minCapacity[sink];
+            v = u;
+        }
+    }
+
+    return maxFlow;
 }
