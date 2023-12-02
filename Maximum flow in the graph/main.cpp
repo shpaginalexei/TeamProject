9e242b349eb4d6a5390e409ac101e88834475a80
SweaD7 <140428847+SweaD7@users.noreply.github.com>
2023-12-02 19:32:08 +0300

    Update main.cpp

--- Maximum flow in the graph/main.cpp
+++ Maximum flow in the graph/main.cpp
@@ -23,12 +23,12 @@ int main() {
 	cout << "\t Graph 1\n";
 	Graph graph1(adj1);
 	Run(ford_fulkerson_method, graph1, 0, 8, "Ford Fulkerson algorithm (by Shpagin Alexei)");
-	// ...
-	
+	Run(FlowScalingAlgorithm, graph1, 0, 8, "Flow Scaling algorithm (by Caxar)");
+	//
 	cout << "\n\t Graph 2\n";
 	Graph graph2(adj2);
 	Run(ford_fulkerson_method, graph2, 4, 7, "Ford Fulkerson algorithm (by Shpagin Alexei)");
-	// ...
-
+	Run(FlowScalingAlgorithm, graph2, 4, 7, "Flow Scaling algorithm (by Caxar)");
+	//
 	return 0;
 }
